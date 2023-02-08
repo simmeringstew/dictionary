bd093091c979b295af10e55311759d1227c854bb
simmeringstew <dev.95a2e@simplelogin.com>
2023-02-08 12:32:54 -0500

    Search component built

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
 import TopBar from "./components/TopBar";
+import Search from "./components/Search";
 import "./App.css";
 
 const App = () => {
+
+  const [search, setSearch] = useState("");
+  const changeSearch = (value) => {
+    setSearch(value.toLowerCase());
+  }
+
   return(
     <div className="container px-4">
       <TopBar />
+      <Search previousSearch={search} changeSearch={changeSearch} />
     </div>
   );
 }
