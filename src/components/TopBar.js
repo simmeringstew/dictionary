fe72a51a84f6f529f53b35e70de25d3afb4fe9aa
simmeringstew <dev.95a2e@simplelogin.com>
2023-02-08 09:55:32 -0500

    User is able to change font type

--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -1,6 +1,32 @@
 import "../styles/TopBar.css";
 
 const TopBar = () => {
+
+    const fonts = [
+        {
+            family: "Merrieweather"
+        },
+        {
+            family: "Lato"
+        },
+        {
+            family: "Source Code Pro"
+        }
+    ];
+
+    const handleChange = (e) => {
+        const font = e.target.value;
+        const body = document.querySelector("BODY");
+        if (font === "serif") {
+            body.style.fontFamily = `${fonts[0].family}, serif`;
+        }
+        else if (font === "sans") {
+            body.style.fontFamily = `${fonts[1].family}, sans-serif`;
+        } else {
+            body.style.fontFamily = `${fonts[2].family}, monospace`;
+        }
+    }
+
     return(
         <div className="bar">
             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" className="bi bi-book" viewBox="0 0 16 16">
@@ -8,19 +34,19 @@ const TopBar = () => {
             </svg>
             <div className="bar-right">
                 <div className="select-box">
-                    <select>
+                    <select onChange={e => handleChange(e)}>
                         <option value="serif">Serif</option>
                         <option value="sans">Sans-Serif</option>
                         <option value="mono">Monospace</option>
                     </select>
                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-chevron-down" viewBox="0 0 16 16">
-                        <path fill-rule="evenodd" d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z"/>
+                        <path fillRule="evenodd" d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z"/>
                     </svg>
                 </div>
-                <label class="switch">
+                <label className="switch">
                     <input type="checkbox" />
-                    <span class="slider"></span>
-                    <span class="switch-text">Light Dark Toggle</span>
+                    <span className="slider"></span>
+                    <span className="switch-text">Light Dark Toggle</span>
                 </label>
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" className="bi bi-moon" viewBox="0 0 16 16">
                     <path d="M6 .278a.768.768 0 0 1 .08.858 7.208 7.208 0 0 0-.878 3.46c0 4.021 3.278 7.277 7.318 7.277.527 0 1.04-.055 1.533-.16a.787.787 0 0 1 .81.316.733.733 0 0 1-.031.893A8.349 8.349 0 0 1 8.344 16C3.734 16 0 12.286 0 7.71 0 4.266 2.114 1.312 5.124.06A.752.752 0 0 1 6 .278zM4.858 1.311A7.269 7.269 0 0 0 1.025 7.71c0 4.02 3.279 7.276 7.319 7.276a7.316 7.316 0 0 0 5.205-2.162c-.337.042-.68.063-1.029.063-4.61 0-8.343-3.714-8.343-8.29 0-1.167.242-2.278.681-3.286z"/>
