a11e4af6f27218c95731869240b1c002adb2940b
simmeringstew <dev.95a2e@simplelogin.com>
2023-02-13 10:13:02 -0500

    Wrote tests for antonyms

--- src/components/Antonyms.js
+++ src/components/Antonyms.js
@@ -6,7 +6,7 @@ const Antonyms = ({ meaning }) => {
         return;
     }
     return (
-        <p className="synonym-antonym-text"><span className="synonym-antonym-title">Antonyms</span>{antonyms.join(", ")}</p>
+        <p className="synonym-antonym-text" data-testid="antonym"><span className="synonym-antonym-title">Antonyms</span>{antonyms.join(", ")}</p>
     );
 }
 
---
+++ src/tests/Antonyms.test.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import Antonyms from "../components/Antonyms";
+
+test("If there are antonyms they are displayed", () => {
+    const meaning = {
+        antonyms: ["test", "another test"]
+    };
+    render(<Antonyms meaning={meaning} />);
+    console.log(meaning);
+    const antonym = screen.getByTestId("antonym");
+    expect(antonym.textContent).toBe("Antonymstest, another test");
+});
+
+test("If there are no antonyms nothing is displayed", () => {
+    const meaning = {
+        antonyms: []
+    };
+    render(<Antonyms meaning={meaning} />);
+    const antonym = screen.queryByText("Antonym");
+    expect(antonym).not.toBeInTheDocument();
+});
